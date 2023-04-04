0a1172ee9dc838cf39de2dae9654a1692de175ec
tepavcevic <djordje.tepa@gmail.com>
2023-04-04 21:52:01 +0200

    Fetching api Added pagination and fetching api Very basic styling so far

---
+++ src/domain/fetchAPI/handleFetch.js
@@ -0,0 +1,29 @@
+const handleFetch = (currentPage) => {
+  const beersPerPage = 20;
+
+  return {
+    fetchRandomBeer: async (currentPage) => {
+      if (typeof currentPage !== "number") {
+        currentPage = 1;
+      }
+
+      try {
+        const response = await fetch(
+          `https://api.punkapi.com/v2/beers?page=${currentPage}&per_page=${beersPerPage}`
+        );
+
+        if (!response.ok) {
+          throw new Error(`Error ${response.status}`);
+        }
+
+        const result = await response.json();
+        return result;
+      } catch (error) {
+        alert(error.message);
+        console.error(error);
+      }
+    },
+  };
+};
+
+export default handleFetch;
